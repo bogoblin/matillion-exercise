154bc3ca78bf5f5766fa7908088a1f55bd41a073
Robert McCann <bogoblin@gmail.com>
2019-05-12 22:58:31 +0100

    Refactor connecting to database into its own method

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -1,11 +1,9 @@
 package main.java;
 
-import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 
 public class Exercise2 {
     private static String USERNAME = "technical_test";
@@ -13,50 +11,73 @@ public class Exercise2 {
     private static String HOSTNAME = "mysql-technical-test.cq5i4y35n9gg.eu-west-1.rds.amazonaws.com";
     private static String DATABASE = "foodmart";
 
+    private static Connection conn;
+
     public static void main(String[] args) {
         // ask for input - department, then pay type, then education level
 
+        try {
+            connect(USERNAME, PASSWORD, HOSTNAME, DATABASE);
+        } catch (SQLException e) {
+            System.err.println("Unable to connect to database, exiting.");
+            System.err.println(e.getMessage());
+            return;
+        }
+
         // query database
-        getEmployees("Store Management", "Monthly", "Bachelors Degree");
+        try {
+            EmployeesResult er = getEmployees(
+                    "Store Management", "Monthly", "Bachelors Degree");
+            //System.out.println(er);
+        } catch (NoConnectionException e) {
+            System.err.println(e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Error executing query.");
+            System.err.println(e.getMessage());
+        }
+
     }
 
-    public static void getEmployees(String department, String payType, String educationLevel) {
+    public static void connect(
+            String username, String password, String hostname, String database)
+            throws SQLException
+    {
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://"+HOSTNAME+"/"+DATABASE, USERNAME, PASSWORD);
+    }
 
-        // Firstly, connect to the database
-        Connection conn = null;
+    public static class NoConnectionException extends Exception {
+        public NoConnectionException(String s) {
+            super(s);
+        }
+    }
 
-        try {
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://"+HOSTNAME+"/"+DATABASE, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            // something has gone wrong with the connection
-            System.err.println("Unable to connect to database.");
-            System.err.println(e);
+    public static EmployeesResult getEmployees(
+            String department, String payType, String educationLevel)
+            throws SQLException, NoConnectionException
+    {
+        if (conn == null) {
+            throw new NoConnectionException(
+                    "No connection to database. Call connect() first.");
         }
 
-        Statement stmt = null;
+        Statement stmt = conn.createStatement();
+        stmt.execute(
+                "PREPARE stmt FROM 'SELECT * " +
+                        "FROM employee " +
+                        "JOIN department " +
+                        "ON department.department_id = employee.department_id " +
+                        "JOIN position " +
+                        "ON position.position_id = employee.position_id " +
+                        "WHERE department_description = ? " +
+                        "AND pay_type = ? " +
+                        "AND education_level = ?';");
+        stmt.execute(String.format("SET @d = '%s';", department));
+        stmt.execute(String.format("SET @p = '%s';", payType));
+        stmt.execute(String.format("SET @e = '%s';", educationLevel));
+        stmt.execute("EXECUTE stmt USING @d, @p, @e;");
 
-        try {
-            stmt = conn.createStatement();
-            stmt.execute(
-                    "PREPARE stmt FROM 'SELECT * " +
-                            "FROM employee " +
-                            "JOIN department " +
-                            "ON department.department_id = employee.department_id " +
-                            "JOIN position " +
-                            "ON position.position_id = employee.position_id " +
-                            "WHERE department_description = ? " +
-                            "AND pay_type = ? " +
-                            "AND education_level = ?';");
-            stmt.execute(String.format("SET @d = '%s';", department));
-            stmt.execute(String.format("SET @p = '%s';", payType));
-            stmt.execute(String.format("SET @e = '%s';", educationLevel));
-            stmt.execute("EXECUTE stmt USING @d, @p, @e;");
-            EmployeesResult er = new EmployeesResult(stmt.getResultSet());
-            System.out.println(er);
-        } catch (SQLException e) {
-            System.err.println(e);
-        }
+        return new EmployeesResult(stmt.getResultSet());
     }
 
     public static class EmployeesResult {
