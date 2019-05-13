6f109fd496d42fecf6d60b3cd1f183fa557e53c1
Robert McCann <bogoblin@gmail.com>
2019-05-13 02:41:40 +0100

    Refactor exercise 2 (starting again)
    I was figuring out how the API worked. Now I understand and
    I can write some better code.

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -7,172 +7,108 @@ public class Exercise2 {
     private static String HOSTNAME = "mysql-technical-test.cq5i4y35n9gg.eu-west-1.rds.amazonaws.com";
     private static String DATABASE = "foodmart";
 
-    private static Connection conn;
-    private static PreparedStatement preparedStatement;
-
     public static void main(String[] args) {
-        // ask for input - department, then pay type, then education level
-
-        // Connect to the database
-        try {
-            connect();
-        } catch (SQLException e) {
-            System.err.println("Unable to connect to database, exiting.");
-            System.err.println(e.getMessage());
-            return;
-        }
-
-        // Prepare the statement to run queries on
-        try {
-            prepareStatement();
-        } catch (SQLException e) {
-            System.err.println("Unable to prepare statement, exiting.");
-            System.err.println(e.getMessage());
-            return;
-        }
-
-        // query database
-        try {
-            EmployeesResult er = getEmployees(
-                    "Store Management", "Monthly", "Bachelors Degree");
-            System.out.println(er);
-        } catch (NoConnectionException e) {
-            System.err.println(e.getMessage());
-        } catch (SQLException e) {
-            System.err.println("Error executing query.");
-            System.err.println(e.getMessage());
-        }
 
     }
 
-    public static void connect() throws SQLException
-    {
-        conn = DriverManager.getConnection(
-                "jdbc:mysql://"+HOSTNAME+"/"+DATABASE, USERNAME, PASSWORD);
-    }
+    public static class EmployeeFinder {
+        private Connection conn;
+        private PreparedStatement preparedStatement;
 
-    public static void prepareStatement() throws SQLException {
-        preparedStatement = conn.prepareStatement(
-                "SELECT employee_id, full_name, position.position_title, " +
-                        "department_description, employee.management_role, salary, " +
-                        "pay_type, education_level " +
-                        "FROM employee " +
-                        "JOIN department " +
-                        "ON department.department_id = employee.department_id " +
-                        "JOIN position " +
-                        "ON position.position_id = employee.position_id " +
-                        "WHERE department_description = ? " +
-                        "AND pay_type = ? " +
-                        "AND education_level = ?;");
-    }
+        public EmployeeFinder(Connection requiredConn) throws SQLException {
+            conn = requiredConn;
 
-    public static class NoConnectionException extends Exception {
-        public NoConnectionException(String s) {
-            super(s);
-        }
-    }
-
-    public static EmployeesResult getEmployees(
-            String department, String payType, String educationLevel)
-            throws SQLException, NoConnectionException
-    {
-        if (conn == null) {
-            throw new NoConnectionException(
-                    "No connection to database. Call connect() first.");
+            // Prepare the statement used to find employees
+            preparedStatement = conn.prepareStatement(
+                    "SELECT employee_id, full_name, position.position_title, " +
+                            "department_description, employee.management_role, salary, " +
+                            "pay_type, education_level " +
+                            "FROM employee " +
+                            "JOIN department " +
+                            "ON department.department_id = employee.department_id " +
+                            "JOIN position " +
+                            "ON position.position_id = employee.position_id " +
+                            "WHERE department_description = ? " +
+                            "AND pay_type = ? " +
+                            "AND education_level = ?;");
         }
 
-        preparedStatement.setString(1, department);
-        preparedStatement.setString(2, payType);
-        preparedStatement.setString(3, educationLevel);
+        public ArrayList<Employee> findEmployees(String department, String payType, String educationLevel)
+        throws SQLException
+        {
+            // Execute the prepared statement with the given arguments
+            preparedStatement.setString(1, department);
+            preparedStatement.setString(2, payType);
+            preparedStatement.setString(3, educationLevel);
+            preparedStatement.execute();
+            ResultSet rs = preparedStatement.getResultSet();
 
-        preparedStatement.execute();
+            ArrayList<Employee> employees = new ArrayList<>();
 
-        return new EmployeesResult(preparedStatement.getResultSet());
-    }
-
-    public static class EmployeesResult {
-
-        public int getColumnCount() {
-            return columnCount;
-        }
-
-        public String[] getColumnLabels() {
-            return columnLabels;
-        }
-
-        public int getNumberOfResults() {
-            return numberOfResults;
-        }
-
-        public ArrayList<String[]> getResults() {
-            return results;
-        }
-
-        private int columnCount;
-        private String[] columnLabels;
-        private int numberOfResults;
-        private ArrayList<String[]> results;
-
-        /**
-         * EmployeesResult stores the result from an SQL query.
-         * @param rs a ResultSet object from an SQL query
-         */
-        public EmployeesResult(ResultSet rs) {
-            try {
-                ResultSetMetaData rsmd = rs.getMetaData();
-                columnCount = rsmd.getColumnCount();
-                columnLabels = new String[columnCount];
-
-                // Store the column labels as a list
-                // Note that result arrays start at 1, so 0th element will be null
-                for (int i=1; i<columnCount; i++) {
-                    columnLabels[i] = rsmd.getColumnLabel(i);
-                }
-
-                // Each row in the result is then stored as an array of strings.
-                // Their values can be indexed, using the same indices from columnLabels.
-                // An list of these is stored.
-
-                numberOfResults = 0;
-                results = new ArrayList<>();
-
-                while (rs.next()) {
-                    String[] currentResult = new String[columnCount];
-                    for (int i=1; i<columnCount; i++) {
-                        currentResult[i] = rs.getString(i);
-                    }
-                    numberOfResults++;
-                    results.add(currentResult);
-                }
-            } catch (SQLException e) {
-                System.err.println(e);
-            } finally {
-                // We can now release the ResultSet
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    System.err.println(e);
-                }
+            while (rs.next()) {
+                employees.add(new Employee(rs));
             }
+
+            return employees;
+        }
+    }
+
+    public static class Employee {
+        private int id;
+        private String fullName;
+        private String positionTitle;
+        private String departmentDescription;
+        private String managementRole;
+        private String salary;
+        private String payType;
+        private String educationLevel;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public String getPositionTitle() {
+            return positionTitle;
+        }
+
+        public String getDepartmentDescription() {
+            return departmentDescription;
+        }
+
+        public String getManagementRole() {
+            return managementRole;
+        }
+
+        public String getSalary() {
+            return salary;
+        }
+
+        public String getPayType() {
+            return payType;
+        }
+
+        public String getEducationLevel() {
+            return educationLevel;
         }
 
         /**
-         * Create a string for printing, showing each employee from the results.
-         * @return
+         * Create an employee from the current result in a result set.
+         * Call rs.next() before using this constructor.
+         * @param rs the result set to create the employee from
          */
-        public String toString() {
-            StringBuilder outputBuilder = new StringBuilder();
-
-            outputBuilder.append(String.format("Found %d results:\n", numberOfResults));
-
-            for (String[] result : results) {
-                outputBuilder.append("------------------------------\n");
-                for (int i=1; i<columnCount; i++) {
-                    outputBuilder.append(columnLabels[i] + ": ");
-                    outputBuilder.append(result[i] + "\n");
-                }
-            }
-            return outputBuilder.toString();
+        public Employee(ResultSet rs) throws SQLException {
+            id = rs.getInt(1);
+            fullName = rs.getString(2);
+            positionTitle = rs.getString(3);
+            departmentDescription = rs.getString(4);
+            managementRole = rs.getString(5);
+            salary = rs.getString(6);
+            payType = rs.getString(7);
+            educationLevel = rs.getString(8);
         }
     }
 }
--- src/test/java/Exercise2Test.java
+++ src/test/java/Exercise2Test.java
@@ -1,35 +1,62 @@
-import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Exercise2Test {
+    @Test
+    public void testCreatingEmployee() {
+        ResultSet rs = mock(ResultSet.class);
 
-    @BeforeClass
-    public static void setup() {
         try {
-            Exercise2.connect();
-            Exercise2.prepareStatement();
-        } catch (SQLException e) {
+            when(rs.getInt(1)).thenReturn(1);
+            when(rs.getString(2)).thenReturn("Test Name");
+            when(rs.getString(3)).thenReturn("Test Position Title");
+            when(rs.getString(4)).thenReturn("Test Department Description");
+            when(rs.getString(5)).thenReturn("Test Management Role");
+            when(rs.getString(6)).thenReturn("4000");
+            when(rs.getString(7)).thenReturn("Test Pay Type");
+            when(rs.getString(8)).thenReturn("Test Education Level");
 
+            Exercise2.Employee test = new Exercise2.Employee(rs);
+            assertEquals(1, test.getId());
+            assertEquals("Test Name", test.getFullName());
+            assertEquals("Test Position Title", test.getPositionTitle());
+            assertEquals("Test Department Description", test.getDepartmentDescription());
+            assertEquals("Test Management Role", test.getManagementRole());
+            assertEquals("4000", test.getSalary());
+            assertEquals("Test Pay Type", test.getPayType());
+            assertEquals("Test Education Level", test.getEducationLevel());
+        } catch (SQLException e) {
+            fail();
         }
     }
 
     @Test
-    // This test is clearly fragile - just using it to check correctness as I go
-    public void integrationTest1() {
+    public void testStatementIsPrepared() {
+        Connection conn = mock(Connection.class);
+
         try {
-            Exercise2.EmployeesResult er = Exercise2.getEmployees(
-                    "Store Management", "Monthly", "Bachelors Degree");
-            assertEquals(8, er.getColumnCount());
-            assertEquals(27, er.getNumberOfResults());
+            Exercise2.EmployeeFinder ef = new Exercise2.EmployeeFinder(conn);
+
+            verify(conn).prepareStatement("SELECT employee_id, full_name, position.position_title, " +
+                    "department_description, employee.management_role, salary, " +
+                    "pay_type, education_level " +
+                    "FROM employee " +
+                    "JOIN department " +
+                    "ON department.department_id = employee.department_id " +
+                    "JOIN position " +
+                    "ON position.position_id = employee.position_id " +
+                    "WHERE department_description = ? " +
+                    "AND pay_type = ? " +
+                    "AND education_level = ?;");
         } catch (SQLException e) {
             fail();
-        } catch (Exercise2.NoConnectionException e) {
-            fail();
         }
-
     }
 }
